c9394944e250d856f0db57f19229c0fc8f75b71d
RubyDEAD <reitzdave.andriano@gmaul.com>
2025-02-26 18:24:31 +0800

    asdasd

---
+++ src/homepage.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react';
+
+
+
+function Dashboard() {
+    return (
+        <div className="body">
+            <div className='container'> 
+                <h1>DASHBOARD</h1>
+            </div>
+        </div>
+    )   
+}
+
+export default Dashboard;
