b6dc1a24d5647cb6a9ed8e47a79174adac2734aa
RubyDEAD <reitzdave.andriano@gmaul.com>
2025-02-26 12:58:13 +0800

    homempage frontend (book service)

---
+++ src/homepage.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import '../src/homepage.css'
+
+
+
+export default function homepage() {
+    return (
+        <div className="body">
+            <div className='container'> 
+                qwewq
+            </div>
+        </div>
+    )   
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Login from './login';
+import Homepage from './homepage';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Login />
+    <Homepage />
   </React.StrictMode>
 );
 // here are the routing or displaying of page
