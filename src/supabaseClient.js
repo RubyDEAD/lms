c9686d33111d2c2f79753df94840d7af1f061297
canal-clarke <23103439@usc.edu.ph>
2025-05-04 21:13:37 +0800

    changed bookservice supabase key

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -5,7 +5,7 @@ const supabaseUrl = 'https://roifumfsdyhyegikikpd.supabase.co'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJvaWZ1bWZzZHloeWVnaWtpa3BkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA4NDQyNzIsImV4cCI6MjA1NjQyMDI3Mn0.h2KoDNHiVPCKnaEWjPADO0TdY4tmO1lGkssjuJDWxbs'
 
 const bookserviceURL = 'https://hwkuzfsecehszlftxqpn.supabase.co'
-const bookKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh3a3V6ZnNlY2Voc3psZnR4cXBuIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0MDYzNzA0OCwiZXhwIjoyMDU2MjEzMDQ4fQ.fDCGoaN6V-U8SXHmIpJqc_LgNj2aLleYd354xxxqBGI'
+const bookKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh3a3V6ZnNlY2Voc3psZnR4cXBuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA2MzcwNDgsImV4cCI6MjA1NjIxMzA0OH0.lCAde3mBhDNZg5kz9ffyF2xYJ2bb7BfOBjC1ZpbIQd4'
 const storageURL = 'https://hwkuzfsecehszlftxqpn.supabase.co/storage/v1/object/public/test/'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
